7ca14f10527203cb1037d42fbb156ed2f8ed9810
Alberto Mallar <alberto.mallar@gmail.com>
2023-09-07 13:39:50 -0300

    (index) seeder modificated

--- PI - Alberto Mallar/server/index.js
+++ PI - Alberto Mallar/server/index.js
@@ -1,14 +1,17 @@
 const server = require('./src/app')
-const {conn} = require('./src/db')
-const {fillDBwithGenres} = require('../server/src/utils/utilsFunctions');
+const { conn } = require('./src/db')
+const { fillDBwithGenres } = require('../server/src/utils/utilsFunctions');
 
 const PORT = 3001;
 
-server.listen(PORT, () => {
-    conn.sync({force:true});
-    console.log('Listening on port 3001');
-    
-    //ACA VOY A LLAMAR A LA FUNCION QUE TRAE TODOS LOS GENEROS DE LA API Y LOS PONE 
-    //EN LA BASE DE DATOS
-    fillDBwithGenres();
-});
+
+conn.sync({ force: false })
+    .then(() => { fillDBwithGenres() })  //ACA VOY A LLAMAR A LA FUNCION QUE TRAE TODOS LOS GENEROS DE LA API Y LOS PONE EN LA BASE DE DATOS
+
+    .then(() => {
+        server.listen(PORT, () => {
+            console.log('Listening on port 3001');
+        })
+    })
+    .catch((error) => console.log('ERROR UP SERVER--->' + error.message));
+
